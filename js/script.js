f2f377dd056c0ad0b5e9bd1c1c9012c537d10a89
Mercynenoh <mercychepngenoh74@gmail.com>
2022-03-12 14:41:56 +0300

    add jquery functions

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+$(".design").toggle();
+});
+$("#dev").click(function(){
+    $(".dev").toggle();
+        });
+      $("#product").click(function(){
+        $(".product").toggle();
+          });
+});
